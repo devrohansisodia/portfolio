3b3fa1353d93882bb14ce76cfb91764a4ebd545e
Rohan Singh <dev.rohansisodia@gmail.com>
2026-06-07 17:33:44 +0530

    Updated profile image

--- src/config/portfolio.js
+++ src/config/portfolio.js
@@ -8,7 +8,7 @@ export const portfolioConfig = {
   newsTechUrl: 'https://github.com/devrohansisodia/newstech',
   linkedinUrl: 'https://www.linkedin.com/in/rohan-singh-bb3359218/',
   resumeUrl: `${base}resume/rohan-singh-resume.pdf`,
-  profileImageUrl: `${base}images/profile.png`,
+  profileImageUrl: `${base}images/profile-2.jpg`,
   email: 'rohan222210@gmail.com',
   emailHref: 'mailto:rohan222210@gmail.com',
   phone: '+91-9971457371',
